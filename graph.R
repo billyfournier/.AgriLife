eb20dc3e3723e42ee613752c5daa95c0e1c34a0b
billyfournier <billyfournier2000@yahoo.com>
2015-02-18 16:27:49 -0600

    edited the y axis label

--- graph.R
+++ graph.R
@@ -2,5 +2,5 @@
 # Making a Plot of backstats.txt data
 # NOTE: The Environmental variables must be exported to work here
 mydata <- read.table(pipe("tail -n $N $PROJECTDIR/bacstats.txt"))
-plot(mydata$V2, type='l', main= "bacstats.txt Curve")
+plot(mydata$V2, type='l', ylab="Sequence Count",main= "bacstats.txt Curve")
 
