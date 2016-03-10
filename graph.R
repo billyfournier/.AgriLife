35f990e2904990b84afb659cd6c042e59a88adaf
billyfournier <billyfournier@users.noreply.github.com>
2016-03-10 14:29:03 -0600

    Changed:   bacstats.txt --> stats.txt

--- graph.R
+++ graph.R
@@ -1,6 +1,6 @@
 
 # Making a Plot of backstats.txt data
 # NOTE: The Environmental variables must be exported to work here
-mydata <- read.table(pipe("tail -n $N $PROJECTDIR/bacstats.txt"))
-plot(mydata$V2, type='l', ylab="Sequence Count / Sample", xlab="Sample Index #",main= "bacstats.txt Curve")
+mydata <- read.table(pipe("tail -n $N $PROJECTDIR/stats.txt"))
+plot(mydata$V2, type='l', ylab="Sequence Count / Sample", xlab="Sample Index #",main= "stats.txt Curve")
 
